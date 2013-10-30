2c9ac9360e6b36b2e418eabcdfe979ee7327c61b
Evan Senter <evansenter@gmail.com>
2013-10-30 19:08:13 -0400

    Hard copy of the libmfpt_header.h file.

--- libmfpt_header.h
+++ libmfpt_header.h
@@ -1,1 +1,32 @@
-/Users/evansenter/Source/rna_kinetics/kinetics/matrix_inverse/c/shared/libmfpt_header.h
+#ifndef LIBMFPT_HEADER_H
+#define LIBMFPT_HEADER_H
+
+typedef struct GlobalParameters {
+  int start_state;
+  int end_state;
+  int sequence_length;
+  short energy_based;
+  short transition_matrix_input;
+  short pseudoinverse;
+  short single_bp_moves_only;
+  short hastings;
+  short verbose;
+  double additive_epsilon;
+  double distributed_epsilon;
+} GlobalParameters;
+
+#ifdef __cplusplus
+  extern "C" {
+    GlobalParameters init_params();
+    int error_handling(GlobalParameters);
+    double** convert_energy_grid_to_transition_matrix(int**, int**, double**, unsigned long*, GlobalParameters);
+    double compute_mfpt(int*, int*, double**, unsigned long, GlobalParameters);
+  }
+#else
+  extern GlobalParameters init_params();
+  int error_handling(GlobalParameters);
+  extern double** convert_energy_grid_to_transition_matrix(int**, int**, double**, unsigned long*, GlobalParameters);
+  extern double compute_mfpt(int*, int*, double**, unsigned long, GlobalParameters);
+#endif
+
+#endif
